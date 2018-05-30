351bdc2d6064395c0536bdadbf28d5838ce82074
yolliero <yves.ollierou@gmail.com>
2018-05-30 15:25:46 +0200

    Début design nav-bar

--- src/app/home/toolbar-toolbar/toolbar-toolbar.component.ts
+++ src/app/home/toolbar-toolbar/toolbar-toolbar.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { AppRoutingModule } from '../../app-routing.module';
+import { Router, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 @Component({
     selector: 'toolbar-toolbar',
     templateUrl: 'toolbar-toolbar.component.html',
     styleUrls: ['toolbar-toolbar.component.scss']
 })
-export class ToolbarToolbarComponent {
+export class ToolbarToolbarComponent   {
+    public href: String = '';
 
-
-    constructor(private translate: TranslateService) {
-        translate.setDefaultLang('en');
-      }
+    constructor(private translate: TranslateService,
+        router: Router) {
+              }
 
       switchLanguage(language: string) {
         this.translate.use(language);
