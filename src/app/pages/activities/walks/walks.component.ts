8e8a889f6c3851b06e1ffe4e5da9f62ff6e87083
yolliero <yves.ollierou@gmail.com>
2018-07-05 14:51:47 +0200

    Correction de Ajout onglet activité et service activité

--- src/app/pages/activities/walks/walks.component.ts
+++ src/app/pages/activities/walks/walks.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
+  
     selector: 'walks',
     templateUrl: 'walks.component.html',
     styleUrls: ['walks.component.scss']
